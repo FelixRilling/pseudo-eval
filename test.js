43f2e4d57073a0e594b8481caba704023b565171
Felix Rilling <contact@f-rilling.com>
2017-06-20 10:34:58 +0200

    re-added math

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 "use strict";
 
 const pseudoEval = require("./index");
-const strings = ["a", "b", "a+b", "a>=b"];
+const strings = ["a", "b", "a+b", "a*b", "b*b","c()===c()"];
 const ctx = {
     a: 1,
-    b: 4
+    b: 4,
+    c: () => 2,
+    d: (a) => a * 2
 };
 
 strings.forEach(string => {

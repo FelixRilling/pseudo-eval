44232c57f6a61861be9215ef4eacc6140f88df74
Felix Rilling <contact@f-rilling.com>
2017-06-20 10:50:12 +0200

    added tests

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,13 +1,63 @@
 "use strict";
 
-const lib = require("../index");
+const pseudoEval = require("../index");
 
-describe("Main tests", function () {
-    it("Simple test", () => {
-        expect(1 + 1).toBe(2);
+describe("Literals", function () {
+    it("Simple test 1", () => {
+        expect(pseudoEval("1")).toBe(1);
     });
 
-    it("Advanced test", () => {
-        expect("fo" + "o").toBe("foo");
+    it("Simple test 2", () => {
+        expect(pseudoEval("'foo'")).toBe("foo");
+    });
+
+    it("Simple test 3", () => {
+        expect(pseudoEval("a", {
+            a: 1
+        })).toBe(1);
+    });
+
+    it("Simple test 4", () => {
+        expect(pseudoEval("a.b", {
+            a: {
+                b: 2
+            }
+        })).toBe(2);
+    });
+});
+
+describe("Comparison", function () {
+    it("Simple test 1", () => {
+        expect(pseudoEval("1===1")).toBe(true);
+    });
+
+    it("Simple test 2", () => {
+        expect(pseudoEval("1!==0")).toBe(true);
+    });
+
+    it("Simple test 3", () => {
+        expect(pseudoEval("'a'==='a'", {
+            a: 1
+        })).toBe(true);
+    });
+
+    it("Simple test 4", () => {
+        expect(pseudoEval("true&&true")).toBe(true);
+    });
+});
+
+describe("Math", function () {
+    it("Simple test 1", () => {
+        expect(pseudoEval("1+1")).toBe(2);
+    });
+
+    it("Simple test 2", () => {
+        expect(pseudoEval("2-1")).toBe(1);
+    });
+
+    it("Simple test 3", () => {
+        expect(pseudoEval("a*a", {
+            a: 2
+        })).toBe(4);
     });
 });
